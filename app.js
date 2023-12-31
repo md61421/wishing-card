f365b6907a97c4909320e04f83dcbdee058865f2
Md Sahil <md61421@gmail.com>
2023-12-31 19:51:03 +0530

    Removed useless code

--- app.js
+++ app.js
@@ -6,19 +6,6 @@ btn.addEventListener("click", function () {
   btn.style.visibility = "hidden";
 });
 
-// let quotes = [
-//   "Embrace the fresh start, cherish moments, and sparkle with positivity. Happy New Year to a year of endless possibilities!",
-//   "New year, new beginnings, new adventures. Cheers to a chapter full of joy, love, and success. Happy New Year!",
-//   "Wishing you a year filled with laughter, love, and incredible achievements. May every day be a new opportunity. Happy New Year!",
-//   "May the upcoming year be a canvas of dreams, painted with vibrant colors of joy and success. Happy New Year!",
-//   "Celebrate the magic of new beginnings. May the coming year bring prosperity, peace, and happiness. Happy New Year!",
-//   "Cheers to a year of growth, positivity, and seizing every opportunity. May success be your constant companion. Happy New Year!",
-//   "New year, new possibilities, new achievements. Embrace the journey ahead with open arms. Wishing you a spectacular New Year!",
-//   "As the clock strikes midnight, let hope reign, dreams soar, and success unfold. Happy New Year filled with endless possibilities!",
-//   "May the New Year be a chapter of triumphs, discoveries, and happiness. Embrace the journey with optimism. Happy New Year!",
-//   "Welcome the New Year with open hearts, bright smiles, and a spirit of adventure. Here's to joy, love, and success!",
-// ];
-
 let quotes = [
   "Reflect on achievements, learn from challenges. Every setback is a setup for a comeback. Embrace the new year with hope and determination.",
   "Celebrate progress, big or small. A new year brings fresh possibilities. Your journey matters, and you have the power to shape your story.",
